7d055f4ae775de0adefe920f785caffe5f0c9280
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-16 13:07:50 -0400

    add update route and function

--- api/views/beans.py
+++ api/views/beans.py
@@ -17,4 +17,29 @@ def create():
   db.session.commit()
   return jsonify(bean.serialize()), 201
 
-  
+@beans.route('/', methods=["GET"])
+def index():
+  beans = Bean.query.all()
+  return jsonify([bean.serialize() for bean in beans]), 200
+
+@beans.route('/<id>', methods=["GET"])
+def show(id):
+  bean = Bean.query.filter_by(id=id).first()
+  bean_data = bean.serialize()
+  return jsonify(bean=bean_data), 200
+
+@beans.route('/<id>', methods=["PUT"])
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  bean = Bean.query.filter_by(id=id).first()
+
+  if bean.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  for key in data:
+    setattr(bean, key, data[key])
+
+  db.session.commit()
+  return jsonify(bean.serialize()), 200
